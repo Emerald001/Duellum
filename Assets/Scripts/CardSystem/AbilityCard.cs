866312d9eee2c46d8d96b32f09874a596ea0f421
Andries <andriesdg199@gmail.com>
2023-06-11 11:28:02 +0200

    chore: start of Card System

---
+++ Assets/Scripts/CardSystem/AbilityCard.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Abilty Card", fileName = "Card")]
+public class AbilityCard : ScriptableObject
+{
+    [Header("Visuals")]
+    public string Name;
+    public Sprite Icon;
+
+    public string Discription;
+
+    [Header("Card Abilities")]
+    public int ManaCost;
+
+    public BaseSelector selector;
+}
