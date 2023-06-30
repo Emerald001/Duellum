dbe7a498e985c4937287d4deb4d82d5120a73b45
Andries <andriesdg199@gmail.com>
2023-06-30 17:41:30 +0200

    chore: updated grid generator

--- Assets/Scripts/GridSystem/Hex.cs
+++ Assets/Scripts/GridSystem/Hex.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hex : MonoBehaviour {
-    [SerializeField] private Renderer renderer;
+    [SerializeField] private new Renderer renderer;
 
     public Material GivenColor { get; set; }
 
