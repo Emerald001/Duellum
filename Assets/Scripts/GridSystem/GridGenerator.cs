5199b38eb35536044deea4425d6003512371a2d0
Andries <andriesdg199@gmail.com>
2023-09-05 18:22:08 +0200

    feat: expanded the GridGenerator to work with Cubes

--- Assets/Scripts/GridSystem/GridGenerator.cs
+++ Assets/Scripts/GridSystem/GridGenerator.cs
@@ -5,9 +5,11 @@ public class GridGenerator : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private Hex HexPrefab;
+    [SerializeField] private Hex CubePrefab;
     [SerializeField] private Hex ExtraHexPrefab;
 
     [Header("Grid Settings")]
+    [SerializeField] private bool isHexagons;
     [SerializeField] private int rings;
     [SerializeField] private int extraRings;
     [SerializeField] private float initialSpawnDelay;
@@ -24,6 +26,9 @@ public class GridGenerator : MonoBehaviour
     private readonly float hexWidth = 1.732f;
     private readonly float hexHeight = 2f;
 
+    private readonly float squareWidth = 1;
+    private readonly float squareHeight = 1;
+
     private Texture2D heightMap;
 
     private void Awake() {
@@ -33,22 +38,63 @@ public class GridGenerator : MonoBehaviour
         GridStaticFunctions.HexWidth = hexWidth;
         GridStaticFunctions.StartPos = startpos;
 
+        GridStaticFunctions.SquareHeight = squareHeight;
+        GridStaticFunctions.SquareWidth = squareWidth;
+
         Parent = new() {
             name = "Grid"
         };
 
-        GenerateGrid();
+        if (isHexagons)
+            GenerateHexGrid();
+        else
+            GenerateSquareGrid();
     }
 
     private readonly List<Hex> Hexes = new();
 
-    private void GenerateGrid() {
+    private void GenerateHexGrid() {
         GridStaticFunctions.RippleThroughGridPositions(new Vector2Int(0, 0), rings + extraRings, (currentPos, i) => {
             Hex hex = Instantiate(i > rings ? ExtraHexPrefab : HexPrefab);
             hex.GridPos = currentPos;
-            hex.transform.position = GridStaticFunctions.CalcWorldPos(currentPos);
+            hex.transform.position = GridStaticFunctions.CalcHexWorldPos(currentPos);
             hex.transform.parent = Parent.transform;
             hex.name = $"Hexagon {currentPos.x}|{currentPos.y}";
+            Hexes.Add(hex);
+        }, false);
+        Hexes.ForEach(x => GridStaticFunctions.Grid.Add(x.GridPos, x));
+
+        int width = (rings + extraRings);
+        Dictionary<Vector2Int, float> positions = new();
+        for (int x = 0; x < heightMap.width; x++) {
+            for (int y = 0; y < heightMap.height; y++) {
+                Color pixel = heightMap.GetPixel(x, y);
+                positions.Add(new Vector2Int(x - width, y - width), pixel.r);
+            }
+        }
+
+        foreach (Hex hex in Hexes) {
+            if (positions.TryGetValue(hex.GridPos, out float value)) {
+                hex.SetBaseColor(new Color(value, value, value));
+                hex.SetActionQueue(new List<Action>() {
+                    new WaitAction(initialSpawnDelay),
+                    new MoveObjectAction(hex.gameObject, initialSpawnSpeed * value, hex.transform.position + new Vector3(0, (value * scaler), 0)),
+                    new DoMethodAction(() => hex.StandardPosition = hex.transform.position)
+                });
+            }
+        }
+
+        Camera.main.transform.position = new Vector3(0, rings * 2, -(rings * 2 + 2));
+        Camera.main.transform.parent.position = new Vector3(0, GridStaticFunctions.Grid[new Vector2Int(0, 0)].transform.position.y, 0);
+    }
+
+    private void GenerateSquareGrid() {
+        GridStaticFunctions.RippleThroughSquareGridPositions(new Vector2Int(0, 0), rings + extraRings, (currentPos, i) => {
+            Hex hex = Instantiate(i > rings ? ExtraHexPrefab : CubePrefab);
+            hex.GridPos = currentPos;
+            hex.transform.position = GridStaticFunctions.CalcSquareWorldPos(currentPos);
+            hex.transform.parent = Parent.transform;
+            hex.name = $"Square {currentPos.x}|{currentPos.y}";
             Hexes.Add(hex);
         }, false);
         Hexes.ForEach(x => GridStaticFunctions.Grid.Add(x.GridPos, x));
