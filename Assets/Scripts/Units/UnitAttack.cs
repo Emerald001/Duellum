f868f1f1cfbbda9405beb15b5798f5200d35b2b2
Diesel <an3s01@hotmail.nl>
2023-09-08 15:56:17 +0200

    feat: created attacks and attributes base code

---
+++ Assets/Scripts/Units/UnitAttack.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[CreateAssetMenu(fileName = "New UnitAttack", menuName = "Unit/UnitAttack")]
+public class UnitAttack : ScriptableObject {
+    public List<Effect> EffectsToApply;
+    public Selector Selector;
+
+    public AbilityType Type;
+    public DamageType damageType;
+
+    public int energyCost;
+    public int Damage;
+    [Range(0, 100)]
+    public int RandomnessRange;
+
+    public string Name;
+    public string Description;
+}
+
+public enum DamageType {
+    Melee,
+    Ranged,
+    Fire,
+    Poison,
+    Frost,
+    Life,
+    Death,
+}
+
+public enum AbilityType {
+    Damage,
+    MapInteraction,
+    Heal,
+}
---
+++ Assets/Scripts/Units/UnitAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "New UnitAttribute", menuName = "Unit/UnitAttribute")]
+public class UnitAttribute : ScriptableObject {
+    public string Name;
+    public string Description;
+
+    public AttributeType type;
+
+    // Resistance, weakness and Immunity
+    public List<RWIData> RWIDatas;
+
+    // Effect Immunity
+    public List<EffectType> ImmuneEffects;
+}
+
+public enum AttributeType {
+    Resistance,
+    Weakness,
+    DamageImmunity,
+    EffectImmunity,
+}
+
+[Serializable]
+public struct RWIData {
+    public DamageType DamageType;
+    public int Severity;
+}
