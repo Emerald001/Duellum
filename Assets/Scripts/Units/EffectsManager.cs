19a4f6e5b29e42abff92c2c884da4405524021f3
Diesel <an3s01@hotmail.nl>
2023-09-08 15:55:10 +0200

    feat: created effects manager

---
+++ Assets/Scripts/Units/EffectsManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public enum EffectType {
+    Burn,
+    Poison,
+    Slow,
+    Drunk,
+    Exhaust,
+    KnockedOut,
+}
+
+public class Effect {
+    public Sprite Icon;
+    public EffectType type;
+
+    public bool canBeStacked;
+
+    public int duration;
+    public int sevarity;
+}
+
+public static class EffectsManager {
+    public static void ApplyEffects(UnitData data, List<Effect> effects) {
+        foreach (var item in effects) {
+            switch (item.type) {
+                case EffectType.Poison:
+                    ApplyPoisonEffect(data, item.sevarity);
+                    break;
+
+                case EffectType.Burn:
+                    // Apply Burn Effect
+                    break;
+            }
+        }
+    }
+
+    private static void ApplyPoisonEffect(UnitData data, int severity) {
+        data.BaseStatBlock.EnergyPoints -= 1 * severity;
+    }
+}
