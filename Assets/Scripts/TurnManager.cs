eb4b98cefcd72b21efbe7b3e3f89b0ea928e7435
Diesel <an3s01@hotmail.nl>
2023-09-12 16:00:59 +0200

    chore: updated Turnmanager

--- Assets/Scripts/TurnManager.cs
+++ Assets/Scripts/TurnManager.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnManager : MonoBehaviour
 {
@@ -11,11 +13,18 @@ public class TurnManager : MonoBehaviour
     [Header("GridSettings")]
     [SerializeField] private OriginalMapGenerator GridGenerator;
 
-    private List<UnitController> livingUnitsInPlay = new();
-    private List<UnitController> unitAttackOrder = new();
-    private UnitController currentUnit = new();
+    private readonly List<UnitController> livingUnitsInPlay = new();
 
-    private int CurrentTurn;
+    private readonly List<UnitController> deadUnitsInPlay = new();
+    private readonly List<UnitController> unitsWithTurnLeft = new();
+
+    private readonly List<UnitController> enemyUnitsInPlay = new();
+    private readonly List<UnitController> playerUnitsInPlay = new();
+
+    private List<UnitController> unitAttackOrder = new();
+    private UnitController currentUnit;
+
+    private int currentTurn;
 
     private UnitFactory unitFactory;
 
@@ -30,20 +39,130 @@ public class TurnManager : MonoBehaviour
         NextUnit();
     }
 
-    private void SpawnUnits() {
-        foreach (var item in GridStaticFunctions.PlayerSpawnPos)
-            AllUnits.Add(unitFactory.CreateUnit(PlaceholderData, item));
+    private void Update() {
+        if (currentUnit == null)
+            return;
 
-        foreach (var item in GridStaticFunctions.EnemySpawnPos)
-            AllUnits.Add(unitFactory.CreateUnit(PlaceholderData, item));
+        if (currentUnit.IsDone) {
+            NextUnit();
+            return;
+        }
+
+        currentUnit.OnUpdate();
+    }
+
+    private void SpawnUnits() {
+        for (int i = 0; i < GridStaticFunctions.PlayerSpawnPos.Count; i++) {
+            Vector2Int item = GridStaticFunctions.PlayerSpawnPos[i];
+            livingUnitsInPlay.Add(unitFactory.CreateUnit(PlayerUnitsToSpawn[i], item));
+        }
+
+        for (int i = 0; i < GridStaticFunctions.EnemySpawnPos.Count; i++) {
+            Vector2Int item = GridStaticFunctions.EnemySpawnPos[i];
+            livingUnitsInPlay.Add(unitFactory.CreateUnit(EnemyUnitsToSpawn[i], item));
+        }
     }
 
     private void OrderUnits() {
-
+        unitAttackOrder = new List<UnitController>(unitsWithTurnLeft.OrderBy(x => x.Values.currentStats.Initiative).Reverse());
     }
 
     private void NextUnit() {
-        currentUnit = UnitsInOrder.Dequeue();
+        if (unitAttackOrder.Count != 0) {
+            //for (int i = 0; i < unitAttackOrder.Count; i++)
+            //    unitAttackOrder[i].gameObject.GetComponentInChildren<Text>().text = (i + 1).ToString();
+
+            if (currentUnit != null) {
+                currentUnit.OnExit();
+                //currentUnit.gameObject.GetComponentInChildren<Text>().text = "-";
+            }
+
+            currentUnit = unitAttackOrder[0];
+            unitAttackOrder.RemoveAt(0);
+            unitsWithTurnLeft.Remove(currentUnit);
+            currentUnit.OnEnter();
+        }
+        else {
+            if (currentUnit != null)
+                currentUnit.OnExit();
+
+            currentUnit = null;
+            NextTurn();
+        }
+    }
+
+    private void NextTurn() {
+        foreach (var unit in livingUnitsInPlay)
+            unitsWithTurnLeft.Add(unit);
+
+        currentTurn++;
+
+        //UIManager.SetInfoText("Turn " + currentTurn.ToString());
+
+        OrderUnits();
+        NextUnit();
+    }
+
+    private void OnExit() {
+        if (enemyUnitsInPlay.Count < 1) {
+            var header = "Win";
+            var body = "You won in " + currentTurn + " turns.";
+
+            //UIManager.ShowEndScreen(header, body);
+        }
+        else if (playerUnitsInPlay.Count < 1) {
+            var header = "Lose";
+            var body = "You lost in " + currentTurn + " turns.";
+
+            //UIManager.ShowEndScreen(header, body);
+        }
+    }
+
+    public void UnitDeath(UnitController unit) {
+        livingUnitsInPlay.Remove(unit);
+        deadUnitsInPlay.Add(unit);
+
+        if (unitAttackOrder.Contains(unit)) {
+            unitsWithTurnLeft.Remove(unit);
+            unitAttackOrder.Remove(unit);
+        }
+
+        OrderUnits();
+
+        if (playerUnitsInPlay.Contains(unit)) {
+            playerUnitsInPlay.Remove(unit);
+
+            if (playerUnitsInPlay.Count < 1)
+                OnExit();
+                //StartCoroutine(ExitDelay(2));
+        }
+        else if (enemyUnitsInPlay.Contains(unit)) {
+            enemyUnitsInPlay.Remove(unit);
+
+            if (enemyUnitsInPlay.Count < 1)
+                OnExit();
+                //StartCoroutine(ExitDelay(2));
+        }
+    }
+
+    public void UnitWait() {
+        if (currentUnit.Values.currentStats.Initiative < 1)
+            return;
+
+        var curUnit = currentUnit;
+        NextUnit();
+        unitsWithTurnLeft.Add(curUnit);
+        curUnit.Values.currentStats.Initiative *= -1;
+        OrderUnits();
+
+        for (int i = 0; i < unitAttackOrder.Count; i++) {
+            unitAttackOrder[i].gameObject.GetComponentInChildren<Text>().text = (i + 2).ToString();
+        }
+    }
+
+    public void UnitEndTurn() {
+        currentUnit.Values.currentStats.Defence *= 2;
+        NextUnit();
     }
 }
 
@@ -53,13 +172,21 @@ public class UnitFactory {
     }
 
     private readonly UnitController prefab;
-     
+    
     public UnitController CreateUnit(UnitData data, Vector2Int spawnPos) {
         UnitController unit = Object.Instantiate(prefab);
 
         unit.transform.position = GridStaticFunctions.CalcSquareWorldPos(spawnPos);
 
-        unit.SetUp(data);
+        unit.SetUp(data, spawnPos);
         return unit;
     }
+}
+
+public enum BattleEvents {
+    SetupBattle,
+    StartBattle,
+    EndUnitTurn,
+    NewTurn,
+    BattleEnd,
 }
