2ba6e3307440bb1f18b96521da26b7d3d297397c
Diesel <an3s01@hotmail.nl>
2023-11-28 16:13:46 +0100

    feat: can grossly start battle in dungeon

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,18 @@
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+    [SerializeField] private BattleData TMPData;
+
     private void Start() {
         EventManager<UIEvents, CursorType>.Invoke(UIEvents.UpdateCursor, CursorType.Normal);
-        Tooltip.HideTooltip_Static();
+        //Tooltip.HideTooltip_Static();
+    }
+
+    private void Update() {
+        if (Input.GetKeyUp(KeyCode.H)) {
+            EventManager<BattleEvents>.Invoke(BattleEvents.StartBattle);
+            EventManager<BattleEvents, BattleData>.Invoke(BattleEvents.StartBattle, TMPData);
+        }
     }
 
     private void Update() {
