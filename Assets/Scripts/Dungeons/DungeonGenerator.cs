d8b6e43fae8f47b5b3db60dfcc2609cbd75cb249
Diesel <an3s01@hotmail.nl>
2023-12-01 17:30:49 +0100

    feat: started line through dungeon

--- Assets/Scripts/Dungeons/DungeonGenerator.cs
+++ Assets/Scripts/Dungeons/DungeonGenerator.cs
@@ -186,6 +186,7 @@ public class DungeonGenerator : MonoBehaviour {
         spawnedRoom.transform.eulerAngles = new(0, room.RotationIndex * 90, 0);
         spawnedRoom.size = room.size;
         spawnedRoom.rotationIndex = room.RotationIndex;
+        spawnedRoom.gridPositionsPerIndex = new(room.GridPositionsPerIndex);
 
         spawnedRoom.gridPositionsPerIndex = new(room.GridPositionsPerIndex);
         spawnedRoom.connections = new(room.connections);
