8110b0e95a48b40ba616d614970da542a892b19f
Andries <andriesdg199@gmail.com>
2023-12-19 15:39:48 +0100

    fix: fixed proper hiding

--- Assets/Shaders/XRayShader/XRaySync.cs
+++ Assets/Shaders/XRayShader/XRaySync.cs
@@ -1,27 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class XRaySync : MonoBehaviour
-{
+public class XRaySync : MonoBehaviour {
     public static int PosID = Shader.PropertyToID("_PlayerPos");
     public static int SizeID = Shader.PropertyToID("_CircleSize");
 
-    public Material WallMaterial;
-    public Camera Camera;
-    public LayerMask Mask;
-    void Update()
-    {
-        var dir = Camera.transform.position - transform.position;
-        var ray = new Ray(transform.position, dir.normalized);
+    [SerializeField] private Material WallMaterial;
+    [SerializeField] private Camera Camera;
+    [SerializeField] private LayerMask Mask;
+
+    private List<Collider> previousHits = new();
+
+    private void Update() {
+        var dir = transform.position - Camera.transform.position;
+        var ray = new Ray(Camera.transform.position, dir.normalized);
         var dis = Vector3.Distance(Camera.transform.position, transform.position);
 
-        if (Physics.Raycast(ray, dis, Mask))
-            WallMaterial.SetFloat(SizeID, 1);
-        else
-            WallMaterial.SetFloat(SizeID, 0);
+        RaycastHit[] tmp = Physics.RaycastAll(ray, dis, Mask);
+        foreach (var item in tmp) {
+            var mat = item.collider.GetComponent<Renderer>().material;
+            mat.SetFloat(SizeID, 1);
+            mat.SetVector(PosID, Camera.WorldToViewportPoint(item.point));
+        }
 
-        var view = Camera.WorldToViewportPoint(transform.position);
-        WallMaterial.SetVector(PosID, view);
+        foreach (var item in previousHits) {
+            if (!tmp.Any(hit => hit.collider == item)) {
+                var mat = item.GetComponent<Renderer>().material;
+                mat.SetFloat(SizeID, 0);
+            }
+        }
+        previousHits = tmp.Select(x => x.collider).ToList();
     }
 }
