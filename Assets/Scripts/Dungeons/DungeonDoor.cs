ae140b10db4de57cc960ab7a8e17d6e59da20caf
Diesel <an3s01@hotmail.nl>
2024-01-08 16:47:22 +0100

    feat: start of dungeon interactables

---
+++ Assets/Scripts/Dungeons/DungeonDoor.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DungeonDoor : MonoBehaviour, IInteractable {
+    public void OnInteract() {
+
+    }
+}
+
+public class DungeonChest : MonoBehaviour, IInteractable {
+    public void OnInteract() {
+
+    }
+}
