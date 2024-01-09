659a0e15f7e78ab05533f5469de4c1b2f379bd7c
Diesel <an3s01@hotmail.nl>
2024-01-09 16:12:50 +0100

    chore: small changes

--- Assets/Scripts/CardSystem/CardHand.cs
+++ Assets/Scripts/CardSystem/CardHand.cs
@@ -46,6 +46,10 @@ public abstract class CardHand : MonoBehaviour {
         cardStack.ResetDeck();
     }
 
+    private void Update() {
+        CardHandStateMachine.OnUpdate();
+    }
+
     protected void GiveCard(int id) {
         if (id != OwnerID)
             return;
