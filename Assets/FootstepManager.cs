83c9e614da4264aa2f031e60ea41f30101377a18
levi <levivermaning@gmail.com>
2024-01-12 05:39:11 +0100

    chore: updated the footstepmanager

--- Assets/FootstepManager.cs
+++ Assets/FootstepManager.cs
@@ -1,20 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FootstepManager : MonoBehaviour {
-    public PlayerController controller;
+    [SerializeField] private List<string> stringList = new List<string>();
+    [SerializeField] private string groundTag = "WalkableTile";
+
     public Transform leftFootTransform;
     public Transform rightFootTransform;
-    public string groundTag = "WalkableTile";
-
-    void Update() {
-
-    }
+    private string previousString;
 
     public void CheckFootstep(Transform footTransform) {
         RaycastHit hit;
         if (Physics.Raycast(footTransform.position, Vector3.down, out hit, 0.1f) && hit.collider.CompareTag(groundTag)) {
-            //footstepSound.pitch = Mathf.Clamp(10, 0.5f, 2.0f);
-            EventManager<AudioEvents, string>.Invoke(AudioEvents.PlayAudio, "HeavyFootsteps");
+
+            string randomString = GetRandomString();
+            Debug.Log("Random String: " + randomString);
+            EventManager<AudioEvents, string>.Invoke(AudioEvents.PlayAudio, randomString);
         }
     }
+    private string GetRandomString() {
+        if (stringList.Count == 0) {
+            Debug.LogWarning("String list is empty!");
+            return null;
+        }
+
+        int randomIndex;
+        do {
+            randomIndex = Random.Range(0, stringList.Count);
+            // Repeat until a different string is selected
+        } while (stringList[randomIndex] == previousString);
+
+        // Store the current string for the next iteration
+        previousString = stringList[randomIndex];
+
+        
+        return stringList[randomIndex];
+    }
+
 }
